51cca2ccb6c70eecec1a8beda62ba13e0fc2c767
SureshP44 <sureshperumal41@gmail.com>
2021-11-15 21:44:19 +0530

    added soc

--- Battle.py
+++ Battle.py
@@ -1,16 +1,16 @@
 import Board
 
-# def PlaceShips(player):
-#     while len(player.ships) != 0:
-#         print('available ships', player.ships)
-#         ship = input('choose the ship')
-#         position = input('choose the position')
-#         player.placeShip(ship, position)
-#         player.displayBoard()
+def PlaceShips(player):
+    while len(player.ships) != 0:
+        print('available ships:=', player.ships)
+        ship = input('{0} - choose the ship...'.format(player.name))
+        position = input('{0} - choose the position...'.format(player.name))
+        player.placeShip(ship, position)
+        player.displayBoard()
 
 def attackInput(player, opponent):
     print('{0} attacks {1}'.format(player.name, opponent.name))
-    position = input('position to attack...')
+    position = input('{0} - Enter the position to attack...'.format(player.name))
     if(position == "show both"):
         player.displayBothBoards(opponent)
         attackInput(player, opponent)
@@ -22,22 +22,14 @@ def attackInput(player, opponent):
 player = [Board.PlayerActions(),Board.PlayerActions()]
 player[0].name = 'Player A'
 player[1].name = 'Player B'
-player[0].placeShip('cruiser', 'a2')
-player[0].placeShip('battleship', 'b3')
-player[0].placeShip('submarine', 'g1')
-player[0].placeShip('destroyer', 'f6')
-player[0].placeShip('carrier', 'i2')
-player[1].placeShip('cruiser', 'i2')
-player[1].placeShip('battleship', 'c4')
-player[1].placeShip('submarine', 'h2')
-player[1].placeShip('destroyer', 'a5')
-player[1].placeShip('carrier', 'j1')
 
-# PlaceShips(player[0])
-# PlaceShips(player[1])
+print('====== PLAYER A ======')
+PlaceShips(player[0])
+print('====== PLAYER B ======')
+PlaceShips(player[1])
 print('----ATTACK-----')
 flag = True
-print((player[0].checkWin()) or (player[1].checkWin()))
+# print((player[0].checkWin()) or (player[1].checkWin()))
 while True:
     if(player[0].checkWin()) or (player[1].checkWin()):
         break
--- Board.py
+++ Board.py
@@ -7,6 +7,9 @@ class Board:
         self.rowMapping = {'a':0, 'b':1, 'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9}
         self.ships = {'carrier':5, 'battleship':4, 'cruiser':3, 'submarine':3, 'destroyer':2}
         self.hitCount = 0
+        self.row = 0
+        self.col = 0
+
     def displayBoard(self):
         print('  1 2 3 4 5 6 7 8 9 10')
         rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
@@ -16,15 +19,13 @@ class Board:
                 print(self.board[i][j],end =" ")
             print('\n', end="")
     
-    def checkSpace(self, type, index):
-        shipLength = 0
-        row = index[0:1]
-        row = self.rowMapping[row]
-        col = int(index[1:len(index)]) #type conversion error if input is 'adfa1'
-        i, j = row, col
+    def checkSpace(self, type):
+        shipLength = self.ships[type]
+        print(self.row)
+        print(self.col)
+        i, j = self.row, self.col-1
         if self.board[i][j] == '-':
-            if j+shipLength <= 11:
-                del self.ships[type]
+            if j+shipLength <= 10:
                 for j in range(j, j+shipLength, 1):
                     if self.board[i][j-1] != '-':
                         return False
@@ -32,67 +33,71 @@ class Board:
             else:
                 return False
         else:
-            print('already occupied')
-
+            return False
+    
+    def checkPostion(self, index):
+        if len(index) == 2 or len(index) == 3:
+            self.row = index[0:1]
+            try:
+                self.col = int(index[1:len(index)]) #type conversion error if input is 'adfa1'
+            except ValueError:
+                return False
+            if self.col < 10:
+                # print('valid row id')
+                try:
+                    self.row = self.rowMapping[self.row]
+                    return True
+                except KeyError:
+                    return False
+            else:
+                print('======== SPACE WAS NOT ENOUGH :( ======')
+                return False
+        return False
 
     def placeShip(self, type, index):
         # to handle invalid length of row.
         shipLength = 0
-        row = index[0:1]
-        col = int(index[1:len(index)]) #type conversion error if input is 'adfa1'
-        if col <=10:
-           # print('valid row id')
-            try:
-                row = self.rowMapping[row]
-                #print('column found')
-            except KeyError:
-                print('incorrect column id')
-        else:
-            print('incorrect row found')
-        print(self.checkSpace(type, index))
+        #print(self.checkSpace(type, index))
         if type in self.ships:
             shipLength = self.ships[type]
-            i, j = row, col
-            print(self.checkSpace(type, index))
-            if self.board[i][j] == '-':
-                if j+shipLength <= 11:
-                    del self.ships[type]
-                    for j in range(j, j+shipLength, 1):
-                        # print(i, end=" ")
-                        # print(j)
-                        self.board[i][j-1] = '*'
-                    print('ship placed.')    
+            if(self.checkPostion(index)):
+                print(self.checkSpace(type))
+                i, j = self.row, self.col
+                if self.checkSpace(type):
+                    if j+shipLength <= 11:
+                        del self.ships[type]
+                        for j in range(j, j+shipLength, 1):
+                            # print(i, end=" ")
+                            # print(j)
+                            self.board[i][j-1] = '*'
+                        print('======== SHIP PLACED !!!! ======')   
+                    else:
+                         print('======== Cannot place here, SHIP LENGTH IS NOT ENOUGH :( ======')
                 else:
-                    print('cant place here, length is not enough')
+                     print('======== That position is already occupied :( ======')
             else:
-                print('already occupied')
-
+                 print('======== That was a INVALID POSITION :( ======')
         else:
-            print('ship not available')
+            print('======== Ship was not available :( ======')
     
 class PlayerActions(Board):
     def attack(self, obj, position):
-        row = position[0:1]
-        col = int(position[1:len(position)]) #type conversion error if input is 'adfa1'
-        if col <=10:
-            try:
-                row = self.rowMapping[row]
-            except KeyError:
-                print('incorrect column id')
+        if(self.checkPostion(position)):
+            i, j = self.row, self.col
+            if obj.board[i][j-1] == '*':
+                print('======== That was a HIT !!!!! ======')
+                obj.board[i][j-1] = '1'
+                self.battleBoard[i][j-1] = '1'
+                self.hitCount = self.hitCount+1
+            elif obj.board[i][j-1] == '1' or obj.board[i][j-1] == '0':
+                print('======== That was a MISS :( ======')
+            else:
+                print('======== That was a MISS :( ======')
+                obj.board[i][j-1] = '0'
+                self.battleBoard[i][j-1] = '0'
         else:
-            print('incorrect row found')
-        i, j = row, col
-        if(obj.board[i][j-1] != '-'):
-            print('its a hit')
-            obj.board[i][j-1] = '1'
-            self.battleBoard[i][j-1] = '1'
-           # obj.displayBoard()
-            self.hitCount = self.hitCount+1
-        else:
-            print('its a miss')
-            obj.board[i][j-1] = '0'
-            self.battleBoard[i][j-1] = '0'
-           # obj.displayBoard()
+            print('======== That was a INVALID POSITION :( ======')
+
 
     def displayBothBoards(self, obj):
         print('\t{0}\t\t\t{1}'.format(self.name, obj.name))
@@ -111,25 +116,8 @@ class PlayerActions(Board):
 
     def checkWin(self):
         if self.hitCount == 17:
-            print('{0} wins', self.name)
+            print('{0} wins'.format(self.name))
             return True
         else:
             return False
 
-
-        
-        
-    
-player = PlayerActions()
-
-
-player.placeShip('cruiser', 'a3')
-# player.placeShip('cruiser', 'c1')
-player.displayBoard()
-# player2 = PlayerActions()
-# player2.placeShip('carrier', 'e2')
-# player2.placeShip('cruiser', 'a3')
-# player2.displayBothBoards(player)
-
-# player2.attack(player, 'd6')
-# # player.attack(player2, 'a3')
